a59e2b3d6eebe1a959ef5ddf77a33c9731ea3a76
Aditya Jinegar <aditya_jinegar@yahoo.com>
2021-06-25 23:59:11 -0400

    Create app.js

---
+++ server/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+
+const schema = require("./schema/schema");
+
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+  })
+);
+
+app.listen(3000, () => {
+  console.log("Now listening for requests on port 4000");
+});
