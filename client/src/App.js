ab6bacfb3b4567de6098d14ca5e81299ecee6401
Aditya Jinegar <aditya_jinegar@yahoo.com>
2021-06-28 18:28:42 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
 import BookList from "./components/BookList";
+
+// apollo client setup
+const client = new ApolloClient({
+  uri: "http://localhost:4000/grapql",
+});
 
 class App extends Component() {
   render() {
     return (
-      <div id="main">
-        <BookList></BookList>
-      </div>
+      <ApolloProvider client={client}>
+        <div id="main">
+          <BookList></BookList>
+        </div>
+      </ApolloProvider>
     );
   }
 }
