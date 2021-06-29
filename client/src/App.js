86348ef7e14a4d0ff55aa5afbcd6484a08c1591f
Aditya Jinegar <aditya_jinegar@yahoo.com>
2021-06-28 20:18:01 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 import BookList from "./components/BookList";
 
 // apollo client setup
 const client = new ApolloClient({
-  uri: "http://localhost:4000/grapql",
+  uri: "http://localhost:3000/graphql",
+  cache: new InMemoryCache(),
 });
 
-class App extends Component() {
+class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
