0ca924d98f6faead67a6d96f3afbff1644f02962
Aditya Jinegar <aditya_jinegar@yahoo.com>
2021-07-01 21:50:03 -0400

    Update BookList.js

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,8 +1,11 @@
 import { useQuery } from "@apollo/client";
+import { useState } from "react";
 import { GET_BOOKS } from "../queries/queries";
+import BookDetails from "./BookDetails";
 
 function BookList() {
   const { loading, error, data } = useQuery(GET_BOOKS);
+  const [selected, setSelected] = useState(null);
 
   if (loading) return "Loading...";
   if (error) return `Error! ${error.message}`;
@@ -10,9 +13,12 @@ function BookList() {
     <div>
       <ul id="book-list">
         {data.books.map((book) => (
-          <li key={book.id}>{book.name}</li>
+          <li key={book.id} onClick={(e) => setSelected(book.id)}>
+            {book.name}
+          </li>
         ))}
       </ul>
+      <BookDetails bookId={selected} data={data}></BookDetails>
     </div>
   );
 }
